e72df4c31d64cc61ddebe5f02a46ab8bf15f5511
tim <tim.ryan@cyberatomics.com>
2019-07-02 13:02:16 -0700

    add config modules needed to declare js global vars

---
+++ client/common.webpack.config.js
@@ -0,0 +1,32 @@
+var globalModules = {
+  jquery: "jQuery"
+};
+
+const importRule = {
+  // Force require global modules
+  test: /.*-(fast|full)opt\.js$/,
+  loader:
+    "imports-loader?" +
+    Object.keys(globalModules)
+      .map(function(modName) {
+        return globalModules[modName] + "=" + modName;
+      })
+      .join(",")
+};
+
+const exposeRules = Object.keys(globalModules).map(function(modName) {
+  // Expose global modules
+  return {
+    test: require.resolve(modName),
+    loader: "expose-loader?" + globalModules[modName]
+  };
+});
+
+const allRules = exposeRules.concat(importRule);
+
+module.exports = {
+  performance: { hints: false },
+  module: {
+    rules: allRules
+  }
+};
