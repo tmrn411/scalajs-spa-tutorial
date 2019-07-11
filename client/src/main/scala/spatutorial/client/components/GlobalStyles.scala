751741f220693552e1b0928d86ca8347b5847647
tim <tim.ryan@cyberatomics.com>
2019-07-11 10:33:33 -0700

    add style to support single button file chooser

--- client/src/main/scala/spatutorial/client/components/GlobalStyles.scala
+++ client/src/main/scala/spatutorial/client/components/GlobalStyles.scala
@@ -8,6 +8,29 @@ object GlobalStyles extends StyleSheet.Inline {
   style(unsafeRoot("body")(
     paddingTop(70.px))
   )
+  
+  /*
+   * this is a shim/hack to display a single buttom that opens a file chooser
+   * https://stackoverflow.com/questions/11235206/twitter-bootstrap-form-file-element-upload-button#18164555
+   * 
+   * it implements this css
+   * note: [] notation is attribute selector, so I think this should match any element tag that has hidden attribute
+   * [hidden] {
+   *   display: none !important;
+   * }
+   * 
+   * scalacss pseudo selectors
+   * https://japgolly.github.io/scalacss/book/features/cond.html
+   * info on display property
+   * https://developer.mozilla.org/en-US/docs/Web/CSS/display
+   * 
+   * This should not be required after migrating to Bootstrap 4
+   */
+  style(unsafeRoot("")(
+      &.attrExists("hidden").not("important")(
+          display.none
+          )
+  ))
 
   val bootstrapStyles = new BootstrapStyles
 }
